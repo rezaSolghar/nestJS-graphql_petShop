ebcb2e7f1551f49187a376aa585d74e1634842ce
pooya <mras.arab@gmail.com>
2024-05-03 19:56:26 +0200

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { PetsModule } from './pets/pets.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+    PetsModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ src/pets/pet.entity.ts
@@ -0,0 +1,11 @@
+import { Field, Int, ObjectType } from '@nestjs/graphql';
+
+@ObjectType()
+export class Pet {
+  @Field((type) => Int)
+  id: number;
+  @Field()
+  name: string;
+  @Field({ nullable: true })
+  type?: string;
+}
